3aabb51b38fa560134d7aa1ea08998a46f711c87
JeongHeon Woo <email@wookingwoo.com>
2023-11-05 05:47:46 +0900

    Add menu_type_preprocess

--- clawler_v2/main.py
+++ clawler_v2/main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 # 데이터 전처리
-def preprocess_for_mongodb(data):
+def data_type_preprocess(data):
     processed_data = []
 
     for entry in data["DS_TB_MNDT_DATEBYMLSVC_5322"]["row"]:
@@ -25,6 +25,55 @@ def preprocess_for_mongodb(data):
     return processed_data
 
 
+def menu_type_preprocess(data):
+    result = []
+    for item in data:
+        # 날짜 추출
+        date = item["dates"]
+
+        # 아침 식사 데이터 추가
+        if item["brst"]:
+            result.append({
+                "date": date,
+                "type": "breakfast",
+                "menu": item["brst"],
+                "calories": item.get("brst_cal", ""),
+                "sum_cal": item.get("sum_cal", "")
+            })
+
+        # 점심 식사 데이터 추가
+        if item["lunc"]:
+            result.append({
+                "date": date,
+                "type": "lunch",
+                "menu": item["lunc"],
+                "calories": item.get("lunc_cal", ""),
+                "sum_cal": item.get("sum_cal", "")
+            })
+
+        # 저녁 식사 데이터 추가
+        if item["dinr"]:
+            result.append({
+                "date": date,
+                "type": "dinner",
+                "menu": item["dinr"],
+                "calories": item.get("dinr_cal", ""),
+                "sum_cal": item.get("sum_cal", "")
+            })
+
+        # 부식 데이터 추가 (해당하는 경우)
+        if item["adspcfd"]:
+            result.append({
+                "date": date,
+                "type": "special",
+                "menu": item["adspcfd"],
+                "calories": item.get("adspcfd_cal", ""),
+                "sum_cal": item.get("sum_cal", "")
+            })
+
+    return result
+
+
 load_dotenv()
 url = os.getenv('API_URL')
 
@@ -32,7 +81,8 @@ response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    mongodb_ready_data = preprocess_for_mongodb(data)
-    print(json.dumps(mongodb_ready_data, indent=2, default=str, ensure_ascii=False))
+    data = data_type_preprocess(data)
+    data = menu_type_preprocess(data)
+    print(json.dumps(data, indent=2, default=str, ensure_ascii=False))
 else:
     print("Error: ", response.status_code)
